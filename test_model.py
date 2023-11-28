8ce0f06197c0746ed12e9f157ca59599a4ee378e
Andrew <ahull@my.bcit.ca>
2023-11-28 11:00:43 -0800

    foundation for tests

---
+++ test_model.py
@@ -0,0 +1,43 @@
+import unittest
+import integratedmodel 
+
+class TestDQN(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_dqn_initialization(self):
+        pass
+
+    def test_dqn_forward_pass(self):
+        pass
+
+class TestDataHandling(unittest.TestCase):
+
+    def test_preprocessing(self):
+        pass
+
+    def test_postprocessing(self):
+        pass
+
+class TestIntegration(unittest.TestCase):
+
+    def test_model_data_interaction(self):
+        pass
+
+class TestFunctionalities(unittest.TestCase):
+
+    def test_training_loop(self):
+        pass
+
+    def test_model_save_load(self):
+        pass
+
+class TestRobustness(unittest.TestCase):
+
+    def test_error_handling(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
